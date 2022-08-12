7d2c80dc04c6a01cb7c1bbe1937a34c7c10e0cdd
Arun <arunacharya255@gmail.com>
2022-08-12 20:07:30 +0545

    added project slot

--- src/api/index.js
+++ src/api/index.js
@@ -21,3 +21,14 @@ export const getAllInfo = client
         return client.query(allData).then(data => data.map((item) => item.data));
     })
     .catch(error => console.error('Error: ', error.message));
+
+export const getAllProjects = client
+    .query(q.Paginate(q.Match(q.Ref('indexes/all_projects'))))
+    .then(response => {
+        const ref = response.data;
+        const allData = ref.map(ref => {
+            return q.Get(ref);
+        });
+        return client.query(allData).then(data => data.map((item) => item.data));
+    })
+    .catch(error => console.error('Error: ', error.message));
